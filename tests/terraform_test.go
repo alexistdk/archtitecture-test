ee95e8e4f28dad2ee11502fb86ff0532048a8d8c
alexistdk <alexisndelgado@gmail.com>
2021-05-17 01:55:32 -0300

    improvement in tests

--- tests/terraform_test.go
+++ tests/terraform_test.go
@@ -2,21 +2,18 @@ package test
 
 import (
 	"testing"
-
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	// "github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestS3Bucket(t *testing.T) {
 	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: "../examples/terraform-hello-world-example",
+		TerraformDir: "../src",
 	})
 
 	defer terraform.Destroy(t, terraformOptions)
 
 	terraform.InitAndApply(t, terraformOptions)
-
-	output := terraform.Output(t, terraformOptions, "hello_world")
-	assert.Equal(t, "Hello, World!", output)
 }
