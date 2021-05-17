63a94ebba762f0c3e6bf38656ffcc83aa90cbb4f
alexistdk <alexisndelgado@gmail.com>
2021-05-17 04:34:35 -0300

    setup of aws' credentials

--- src/main.tf
+++ src/main.tf
@@ -8,8 +8,14 @@ terraform {
   }
 }
 
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "aws_region_default" {}
+
 provider "aws" {
-  region = "us-west-1"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region = var.aws_region_default
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
