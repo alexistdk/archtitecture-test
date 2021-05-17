20a32f5dec17999a0bddd7281e6340957b9abb74
alexistdk <alexisndelgado@gmail.com>
2021-05-17 11:02:34 -0300

    remove unnecessary dependencies

--- tests/terraform_test.go
+++ tests/terraform_test.go
@@ -3,8 +3,6 @@ package test
 import (
 	"testing"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	// "github.com/gruntwork-io/terratest/modules/aws"
-	// "github.com/stretchr/testify/assert"
 )
 
 func TestS3Bucket(t *testing.T) {
